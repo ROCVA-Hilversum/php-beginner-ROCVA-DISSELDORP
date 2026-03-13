1c1eb0a1ff879085e566ebfa941ee01a7ff9fe93
Lucas Disseldorp <lucas.secondary@disseldorp.one>
2026-03-13 17:17:59 +0100

    sdafsad

--- level1/opdrachten/opdracht-1.1.php
+++ level1/opdrachten/opdracht-1.1.php
@@ -1,14 +1,3 @@
 <?php
 
-// Opdracht 1 – Welkomstbericht
-//
-// Schrijf in dit bestand een PHP‑script dat precies deze tekst op het scherm print:
-//
-// PHP is leuk!
-//
-// Requirements:
-// - Gebruik één of meer `echo`‑statements.
-// - Je hoeft (en mag) hier nog geen eigen functies schrijven.
-// - Het programma hoeft geen input te vragen.
-
-
+echo "PHP is leuk!";
--- level1/opdrachten/opdracht-1.2.php
+++ level1/opdrachten/opdracht-1.2.php
@@ -1,13 +1,8 @@
 <?php
 
-// Opdracht 2 – Herhaal een boodschap
-//
-// Maak in dit bestand een variabele `$boodschap` met een korte tekst, bijvoorbeeld:
-// "Ik leer PHP".
-//
-// Print daarna de waarde van `$boodschap` drie keer onder elkaar.
-//
-// Tips:
-// - Gebruik `echo` om tekst te printen.
-// - Gebruik `PHP_EOL` om een nieuwe regel te maken.
-// - Je hoeft geen eigen functies te schrijven; alles mag in dit script staan.
+$boodschap = "Ik leer PHP";
+
+echo $boodschap . PHP_EOL;
+echo $boodschap . PHP_EOL;
+echo $boodschap . PHP_EOL;
+
